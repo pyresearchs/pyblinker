ae7fbd79cefbc1f10a4028dd0b65054d97866bec
RODNEY PETRUS BALANDONG <balandongiv@gmail.com>
2026-01-13 14:49:43 +0800

    Restore BlinkProperties pipeline entry point

--- pyblinker/pipeline_steps.py
+++ pyblinker/pipeline_steps.py
@@ -12,12 +12,8 @@ from tqdm import tqdm
 from pyblinker.logging import get_logger
 from pyblinker.utils.statistics_utils import get_good_blink_mask, get_blink_statistic
 from pyblinker.blinker.fit_blink import FitBlinks
-from pyblinker.blink_features._blink_metrics_shared import normalize_modality
-from pyblinker.blink_features.kinematics.core_metrics import (
-    compute_blink_kinematic_properties,
-)
-from pyblinker.blink_features.morphology.core_metrics import (
-    compute_blink_morphology_properties,
+from pyblinker.blink_features.waveform_features.extract_blink_properties import (
+    BlinkProperties,
 )
 from pyblinker.blinker.get_blink_positions import get_blink_position
 from pyblinker.blinker.get_representative_channel import channel_selection
@@ -69,24 +65,13 @@ def process_channel_data(detector, channel: str, verbose: bool = True) -> None:
         logger.warning("No good blinks found in channel: %s", channel)
         return
     # STEP 5: Compute blink properties
-    df_out = df.copy()
-    modality_key = normalize_modality(detector.params.get("modality"))
-    candidate_signal = detector.raw_data.get_data(picks=channel)[0]
-    compute_blink_morphology_properties(
-        df_out,
-        candidate_signal,
+    df_in = df.copy()
+    df_out = BlinkProperties(
+        detector.raw_data.get_data(picks=channel)[0],
+        df_in,
         detector.params["sfreq"],
-        modality=modality_key,
-        shut_amp_fraction=detector.params["shut_amp_fraction"],
-        fitted=True,
-    )
-    compute_blink_kinematic_properties(
-        df_out,
-        candidate_signal,
-        detector.params["sfreq"],
-        modality=modality_key,
-        fitted=True,
-    )
+        detector.params,
+    ).df
     # df_out=df.copy()
     # from pyblinker.utils.blinker_feature import save_blinkprops_pickle,load_blinkprops_pickle,replay_and_assert_blinkprops
     # save_blinkprops_pickle(
