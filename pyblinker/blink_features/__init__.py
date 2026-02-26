363a62593b450a28f2d0cf2771a61c8ccf2a03e5
rpb <balandongiv@gmail.com>
2026-02-26 22:49:03 +0800

    file to remove

--- pyblinker/blink_features/__init__.py
+++ pyblinker/blink_features/__init__.py
@@ -1,5 +1,5 @@
 """Blink feature modules package."""
 
-from .aggregate import aggregate_blink_features
+# from .aggregate import aggregate_blink_features
 
-__all__ = ["aggregate_blink_features"]
+# __all__ = ["aggregate_blink_features"]
