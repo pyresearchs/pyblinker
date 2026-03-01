6b45e9811ef76b8528afa097fc645df9be42eef5
rpb <balandongiv@gmail.com>
2026-03-01 22:01:37 +0800

    fix: update version number from 0.3.2 to 0.3.8 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyblinker",
-    version="0.3.2",
+    version="0.3.8",
     description="An open-source alternative to MATLAB BLINKER, integrated with MNE-Python, supporting multimodal biosignal research with flexible workflows, reproducible metrics, and enhanced accessibility.",
     author="rpb",
     packages=["pyblinker", "pyblinker.utilities", "pyblinker.vislab", "pyblinker.viz"],
